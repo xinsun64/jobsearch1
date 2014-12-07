13903434b6679b3914618034ce813627c7c891dd
Xiaozhong Zhang <zhangxiaozhong@gmail.com>
2014-12-06 19:48:16 -0500

    Added company overview and blog

--- jobsearch/app/controllers/map_controller.rb
+++ jobsearch/app/controllers/map_controller.rb
@@ -4,5 +4,8 @@ class MapController < ApplicationController
   	@jobs = session[:jobs]
   	@comps = Map.get_comps(@jobs)
   	gon.jobs = @jobs
+  	
+  	@permalinks = Map.get_permalinks(@comps)
+  	gon.permalinks = @permalinks
   end
 end
--- jobsearch/app/models/map.rb
+++ jobsearch/app/models/map.rb
@@ -4,6 +4,7 @@ require 'net/http'
 require 'rexml/document'
 require 'xmlsimple'
 require 'set'
+include REXML
 
 class Map < ActiveRecord::Base
 	def self.get_jobs
@@ -19,5 +20,24 @@ class Map < ActiveRecord::Base
 		}
 		@comps = s.to_a.sort_by{|word| word.downcase}
 	end
-  
+
+	def self.get_permalinks(comps)
+		h = Hash.new
+		comps.each { |comp|
+			begin
+				c = comp.gsub(/[ &]/, '+')
+				uri = URI('http://api.crunchbase.com/v/2/organizations?query=' + c + '&user_key=300f7644a14557a314f82154661c941a&page=1')
+				json = JSON.parse(Net::HTTP.get(uri))
+				permalink = json['data']['items'][0]['path']
+				# uri = URI('http://api.crunchbase.com/v/2/'+ permalink + '?user_key=300f7644a14557a314f82154661c941a')
+				# json = JSON.parse(Net::HTTP.get(uri))
+				# description = json['data']['properties']['description']
+				h[comp] = permalink
+			rescue
+				# pass
+			end
+		}
+		return h
+	end
+
 end
